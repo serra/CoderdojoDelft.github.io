fbea88e15a39bf4795bd52a67c53455aea01b8da
Erna Oudman <ernaoudman@hotmail.com>
2019-02-20 21:44:34 +0100

    dates dojo's 2019

--- src/content/next-edition.js
+++ src/content/next-edition.js
@@ -1,5 +1,58 @@
 export const nextEdition = {
     date: 'zaterdag 23 februari 2019',
-    registrationStart: 'zondag 10 februari',
+    registrationStart: 'zondag 10 februari2019',
     registrationUrl: '//eventbrite.nl/tickets-external?eid=55768330578'
 };
+
+export const nextEditions = [
+    {
+        date: 'zaterdag 23 februari 2019',
+        registrationStart: 'zondag 10 februari 2019',
+        registrationUrl: '//eventbrite.nl/tickets-external?eid=55768330578'
+    },
+    {
+        date: 'zaterdag 23 maart 2019',
+        registrationStart: 'zondag 10 maart 2019',
+        registrationUrl: '//eventbrite.nl/tickets-external?eid=57038658162'
+    },
+    {
+        date: 'zaterdag 20 april 2019',
+        registrationStart: 'zondag 7 april 2019',
+        registrationUrl: '//eventbrite.nl/tickets-external?eid=57043430436'
+    },
+    {
+        date: 'zaterdag 25 mei 2019',
+        registrationStart: 'zondag 12 mei 2019',
+        registrationUrl: '//eventbrite.nl/tickets-external?eid=57043551799'
+    },
+    {
+        date: 'zaterdag 22 juni 2019',
+        registrationStart: 'zondag 9 juni 2019',
+        registrationUrl: '//eventbrite.nl/tickets-external?eid=57043703252'
+    },
+    {
+        date: 'zaterdag 20 juli 2019',
+        registrationStart: 'zondag 7 juli 2019',
+        registrationUrl: '//eventbrite.nl/tickets-external?eid=57043915888'
+    },
+    {
+        date: 'zaterdag 28 september 2019',
+        registrationStart: 'zondag 15 september 2019',
+        registrationUrl: '//eventbrite.nl/tickets-external?eid=57044040260'
+    },
+    {
+        date: 'zaterdag 26 oktober 2019',
+        registrationStart: 'zondag 13 oktober 2019',
+        registrationUrl: '//eventbrite.nl/tickets-external?eid=57044186698'
+    },
+    {
+        date: 'zaterdag 23 november 2019',
+        registrationStart: 'zondag 10 november 2019',
+        registrationUrl: '//eventbrite.nl/tickets-external?eid=57044346175'
+    },
+   {
+        date: 'zaterdag 21 december 2019',
+        registrationStart: 'zondag 8 december 2019',
+        registrationUrl: '//eventbrite.nl/tickets-external?eid=57044489604'
+    }
+]
